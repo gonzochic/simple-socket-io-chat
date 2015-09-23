2218e98cf8c985f0009100d680a8d257d24c2984
Roman Schiefer <roman.schiefer@gmail.com>
2015-09-23 23:03:54 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express'),
+  socket = require('./routes/socket.js');
+
+var app = module.exports = express.createServer();
+
+// Hook Socket.io into Express
+var io = require('socket.io').listen(app);
+
+io.sockets.on('connection', socket);
+
+// Start server
+
+app.listen(3000, function(){
+  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+});
